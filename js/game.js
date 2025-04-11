d609966d12ed92d124361831b32cf48070d71c2a
benjamin-abebe <150177783+benjamin-abebe-02@users.noreply.github.com>
2025-04-11 16:42:20 +0000

    innit

---
+++ js/game.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Benjamin Abebe
+// Created on: Apr 2025
+// This file contains the JS functions for index.html
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a,
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
