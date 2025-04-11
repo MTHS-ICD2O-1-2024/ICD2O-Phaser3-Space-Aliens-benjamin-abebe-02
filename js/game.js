a3f636b46261f3eb39a99deb9c1abb85eef436a3
benjamin-abebe <150177783+benjamin-abebe-02@users.noreply.github.com>
2025-04-11 17:15:10 +0000

    innit

--- js/game.js
+++ js/game.js
@@ -4,11 +4,17 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
   backgroundColor: 0x5f6e7a,
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH, 
-  }
+  },
 }
 
 const game = new Phaser.Game(config)
