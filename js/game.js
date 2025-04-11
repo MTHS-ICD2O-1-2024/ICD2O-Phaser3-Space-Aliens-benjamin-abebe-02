b4e56cc2f954722bce37604c2019b32406fe1e89
benjamin-abebe <150177783+benjamin-abebe-02@users.noreply.github.com>
2025-04-11 16:59:17 +0000

    innit

--- js/game.js
+++ js/game.js
@@ -1,8 +1,4 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Benjamin Abebe
-// Created on: Apr 2025
-// This file contains the JS functions for index.html
+/* global Phaser */
 
 const config = {
   type: Phaser.AUTO,
---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Benjamin Abebe
+// Created on: Apr 2025
+// This file contains the JS functions for index.html
