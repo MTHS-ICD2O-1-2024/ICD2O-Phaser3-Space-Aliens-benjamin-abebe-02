57f8461155110307c962fbe850d17a9b44dba4bb
benjamin-abebe <150177783+benjamin-abebe-02@users.noreply.github.com>
2025-04-11 17:04:48 +0000

    innit

--- js/game.js
+++ js/game.js
@@ -5,6 +5,10 @@ const config = {
   width: 1920,
   height: 1080,
   backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH, 
+  }
 }
 
 const game = new Phaser.Game(config)
