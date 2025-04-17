bfe3d13dbe2fc78a67e3f1c7d3f9a60801efbecc
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-04-17 17:11:30 +0000

    innit

--- js/game.js
+++ js/game.js
@@ -1,5 +1,17 @@
 /* global Phaser */
 
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Benjamin Abebe
+// Created on: Apr 2025
+// This file contains the JS functions for index.html
+
+import SplashScene from './splashScene.js'
+
+// Our game scene
+const splashScene = new SplashScene()
+
+//* Game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
@@ -7,15 +19,21 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      debug: true
-    }
+      debug: true,
+    },
   },
   backgroundColor: 0x5f6e7a,
   scale: {
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
 
 const game = new Phaser.Game(config)
-console.log(game)
+// console.log(game)
+
+// load scenes
+game.scene.add('splashscene', splashScene)
+
+// start title
+game.scene.start('splashscene')
