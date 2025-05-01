9b7dc28f24602d13b7f1a14d72ea50d07c836430
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-05-01 17:26:35 +0000

    innit

--- js/game.js
+++ js/game.js
@@ -5,6 +5,7 @@
 // Created on: Apr 2025
 // This file contains the JS functions for index.html
 
+
 // scene import statements
 import SplashScene from "./splashScene.js"
 import TitleScene from "./titleScene.js"
@@ -14,31 +15,32 @@ import TitleScene from "./titleScene.js"
 const splashScene = new SplashScene()
 const titleScene = new TitleScene()
 
-
-
 const config = {
-  type: Phaser.AUTO,
-  width: 1920,
-  height: 1080,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      debug: true,
-    },
+type: Phaser.AUTO,
+width: 1920,
+height: 1080,
+physics: {
+  default: 'arcade',
+  arcade: {
+    debug: true,
   },
-  backgroundColor: 0x5f6e7a,
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-  },
+},
+backgroundColor: 0x5f6e7a,
+scale: {
+  mode: Phaser.Scale.FIT,
+  autoCenter: Phaser.Scale.CENTER_BOTH,
+},
 }
+
 
 const game = new Phaser.Game(config)
 // console.log(game)
 
+
 // load scenes
 game.scene.add('splashscene', splashScene)
 game.scene.add('titlescene', titleScene)
+
 
 // start title
 game.scene.start('splashscene')
