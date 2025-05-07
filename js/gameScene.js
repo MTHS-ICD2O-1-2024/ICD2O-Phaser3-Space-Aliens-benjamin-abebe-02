64ddf5af9231395aa0b76a8cdc98a2afce6ea267
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-05-07 17:17:18 +0000

    innit

--- js/gameScene.js
+++ js/gameScene.js
@@ -1,70 +1,79 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Benjamin Abebe
+/* global Phaser */
+
+// Created by: Bain Liao
 // Created on: Apr 2025
-// This file contains the JS functions for index.html
-
+// This is the Game Scene
 
 /**
-* This class is the Menu Scene.
-*/
-
+ * This class is the Game Scene.
+ */
 class GameScene extends Phaser.Scene {
-/**
-  * This method is the constructor.
-  */
+  /**
+   * This method is the constructor.
+   */
+  constructor () {
+    super({ key: 'gameScene' })
 
-constructor () {
-  super({ key: "gameScene" })
+    this.background = null
+    this.ship = null
+  }
 
-  this.background = null
-  this.ship = null
-}
+  /**
+   * Can be defined on your own Scenes.
+   * This method is called by the Scene Manager when the scene starts,
+   *  before preload () and create ().
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  init (data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
 
-/**
-  * Can be defined on your own Scenes.
-  * This method is called by the Scene Manager when the scene starts
-  *  before preload () and create ().
-  * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-  */
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to load assets.
+   */
+  preload () {
+    console.log('Game Scene')
 
-init (data) {
-  this.cameras.main.setBackgroundColor('#ffffff')
-}
+    this.load.image('starBackground', './assets/starBackground.png')
+    this.load.image('ship', './assets/spaceShip.png')
+  }
 
-/**
-  * Can be defined on your own Scenes.
-  * Use it to load assets.
-  */
-preload() {
-  console.log("Game Scene")
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to create your game objects.
+   * @param {object} data Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  create (data) {
+    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+    this.background.setOrigin(0, 0)
 
-  this.load.image("starBackground", "./assets/starBackground.png")
-  this.load.image("ship", "./assets/spaceShip.png")
-}
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+  }
 
-/**
-  * Can be defined on your own Scenes.
-  * Use it to create your game objects.
-  * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-  */
-create (data) {
-  this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
-  this.background.setOrigin(0, 0)
+  /**
+   * Should be overridden by your own Scenes.
+   * This method is called once per game step while the scene is running.
+   * @param {number} time The current time.
+   * @param {number} delta The delta time in ms since the last frame.
+   */
+  update (time, delta) {
+    const keyLeftObj = this.input.keyboard.addKey('LEFT')
+    const keyRightObj = this.input.keyboard.addKey('RIGHT')
 
-  this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
-}
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 15
+      if (this.ship.x < 0) {
+        this.ship.x = 0
+      }
+    }
 
-
-/**
-  * Should be overridden by your own Scenes.
-  * This method is called once per game step while the scene is running.
-  * @param {number} time - The current time.
-  * @param {number} delta - The delta time in ms since the last frame.
-  */
-
-update (time, delta) {
-  // pass
+    if (keyRightObj.isDown === true) {
+      this.ship.x += 15
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
+      }
+    }
   }
 }
 
